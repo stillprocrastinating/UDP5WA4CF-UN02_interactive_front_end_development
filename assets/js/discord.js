1653252f0793558385ff4b686a9932779302d84a
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-08-11 22:30:07 +0100

    Project documents setup

---
+++ assets/js/discord.js
@@ -0,0 +1,5 @@
+function discord() {
+    alert("Feel free to drop me a message! My Discord name is .stillprocrastinating");
+}
+let buttonDiscord = document.querySelector("button.discord");
+buttonDiscord.addEventListener("click", discord);
