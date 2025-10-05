48ed1b1ac50593e098117924148d9424bf16023c
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-10-05 09:52:38 +0100

    Implement discord.js

---
+++ assets/js/discord.js
@@ -0,0 +1,5 @@
+function discord() {
+    alert("Feel free to drop me a message!\nMy Discord name is .stillprocrastinating");
+}
+let buttonDiscord = document.getElementById("discord");
+buttonDiscord.addEventListener("click", discord);
